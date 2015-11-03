8b07e40ad3dbaa6f9987a13f79ffc02c106b729c
Chuka Ebi <chuka.ebi@outlook.com>
2015-11-03 18:08:14 +0000

    Added a page to submit new links

--- app/app.rb
+++ app/app.rb
@@ -1,10 +1,26 @@
 require 'sinatra/base'
+require_relative './models/link'
+
 class BookmarkManager < Sinatra::Base
-  get '/views' do
+
+  get '/' do
+    redirect '/links'
+  end
+
+  get '/links' do
     @links = Link.all
     erb(:views)
   end
 
+  get '/links/new' do
+    erb(:new_link)
+  end
+
+  post '/links' do
+    Link.create(:url => params[:url], :title => params[:title])
+    @links = Link.all
+    redirect('/links')
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
