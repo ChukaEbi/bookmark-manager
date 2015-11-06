d742920faaa2afb44a909fe6726aea6fc45b8bc8
Chuka Ebi <chuka.ebi@outlook.com>
2015-11-06 13:42:23 +0000

    User database encrypts passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,21 @@
+require 'bcrypt'
+
 class User
   include DataMapper::Resource
+  include BCrypt
 
   property :id, Serial
   property :user_name, String
   property :email, String
-  property :password, String
+  property :password_hash, String, length: 80
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
 end
